8d162c0bd48bbb41d53d78506bace286ddf0bee1
wmmc <wmmclarke@gmail.com>
2014-09-03 18:20:21 +0100

    added instance variables to clear up local clutter

--- cw.rb
+++ cw.rb
@@ -7,8 +7,8 @@ HEIGHT = 3
 WIDTH = 5
 NUMBER_OF_BLANK_SQUARES = 7
 
-def print_grid(grid)
-	grid.each do |i|
+def print_grid
+	@grid.each do |i|
 		p i.join(' ')
 	end
 end
@@ -17,40 +17,40 @@ def random_square
 	[(rand HEIGHT), (rand WIDTH)]
 end
 
-def add_random_blank_squares(number_of_blanks, grid)
+def add_random_blank_squares(number_of_blanks)
 	number_of_blanks.times do |square|
 		new_square = random_square
-		if (grid[new_square[0]][new_square[1]] == '■')
+		if (@grid[new_square[0]][new_square[1]] == '■')
 			redo
 		else
-			grid[new_square[0]][new_square[1]] = '■'
+			@grid[new_square[0]][new_square[1]] = '■'
 		end
 	end
 end
 
-def neighbours(h, w, grid)
+def neighbours(h, w)
 	return_array = []
 # 	neighbours = [-1,0,1].product([-1,0,1]) - [0,0] #=> [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 0], [0, 1], [1, -1], [1, 0], [1, 1]]
-	# return_array << grid[h+1][w+1] 	unless ((h+2 > HEIGHT) || (w+1 < WIDTH)) # diagonal
-	return_array << grid[h+1][w] 	unless ( h+2 > HEIGHT) 
-	# return_array << grid[h+1][w-1] 	unless ((h+2 > HEIGHT )|| (w-1 < 0)) # diagonal
-	return_array << grid[h][w+1] 	unless (					w+1 > WIDTH)
-	# return_array << grid[h][h]    #Commented out because it's the current square.
-	return_array << grid[h][w-1] 	unless (					w-1 < 0	)
-	# return_array << grid[h-1][w+1] 	unless ((h-1 < 0) 	|| (w+1 > WIDTH)) # diagonal
-	return_array << grid[h-1][w]  	unless (	h-1 < 0	)
-	# return_array << grid[h-1][w-1]	  unless ((h-1 < 0) 	|| (w-1 < 0)) # diagonal
+	# return_array << @grid[h+1][w+1] 	unless ((h+2 > HEIGHT) || (w+1 < WIDTH)) # diagonal
+	return_array << @grid[h+1][w] 	unless ( h+2 > HEIGHT) 
+	# return_array << @grid[h+1][w-1] 	unless ((h+2 > HEIGHT )|| (w-1 < 0)) # diagonal
+	return_array << @grid[h][w+1] 	unless (					w+1 > WIDTH)
+	# return_array << @grid[h][h]    #Commented out because it's the current square.
+	return_array << @grid[h][w-1] 	unless (					w-1 < 0	)
+	# return_array << @grid[h-1][w+1] 	unless ((h-1 < 0) 	|| (w+1 > WIDTH)) # diagonal
+	return_array << @grid[h-1][w]  	unless (	h-1 < 0	)
+	# return_array << @grid[h-1][w-1]	  unless ((h-1 < 0) 	|| (w-1 < 0)) # diagonal
 	return_array.compact
 
 end
 
-def remove_strange_blank_squares(grid)
-	# grid[1][1]= '?'
-	grid.each_with_index do |height, height_index|
+def remove_strange_blank_squares
+	# @grid[1][1]= '?'
+	@grid.each_with_index do |height, height_index|
 		height.each_with_index do |width, width_index|
-			n = neighbours(height_index, width_index, grid)
+			n = neighbours(height_index, width_index)
 			if n == ['■','■'] || n == ['■','■','■'] || n == ['■','■','■','■'] || n == ['■','■','■','■','■']
-				grid[height_index][width_index] = '■'
+				@grid[height_index][width_index] = '■'
 			end
 		end
 	end
@@ -59,24 +59,47 @@ end
 
 
 
-def add_borders_to_grid(grid)
-	updated_grid = grid.dup
-	updated_grid.unshift([('#' * WIDTH).split(//)]) # Top Line
-	updated_grid << [('#' * WIDTH).split(//)]		# Bottom Line
-	updated_grid.each do |line|
+def add_borders_to_grid
+	@grid.unshift([('#' * WIDTH).split(//)]) # Top Line
+	@grid << [('#' * WIDTH).split(//)]		# Bottom Line
+	@grid.each do |line|
 		line.unshift '#'							# Left Column
 		line << '#'									# Right Column
 	end
 
 end
 
-grid = Array.new(HEIGHT) { Array.new(WIDTH) { '.' } }
-add_random_blank_squares(NUMBER_OF_BLANK_SQUARES, grid)
-print_grid grid
+def find_the_starting_points_for_words
+	@grid.each_with_index do |height, height_index|
+		height.each_with_index do |width, width_index|
+			next unless @grid[height_index][width_index] == '.'
+			if height_index == 0 || @grid[height_index-1][width_index] == '■' #|| # if the cell above is a border 
+				# We're working here... Hmmm...
+			end
+		end
+	end
+end
 
-remove_strange_blank_squares grid
 
-updated_grid = add_borders_to_grid grid
+def drop_outer_blanks
+	# @grid = 
+
+end
+
+
+@grid = Array.new(HEIGHT) { Array.new(WIDTH) { '.' } }
+add_random_blank_squares(NUMBER_OF_BLANK_SQUARES)
+print_grid 
+
+remove_strange_blank_squares
+
+drop_outer_blanks
+
+find_the_starting_points_for_words
+
+
+add_borders_to_grid
+
 p ''
-print_grid(updated_grid)
+print_grid
 
